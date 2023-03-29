cd697417f40dde0ae7fac91a1ec2644e164eb3b8
spokesuger <raspbkaito0924@gmail.com>
2023-03-30 06:39:58 +0900

    change web app

--- lib/router/rotes/license.part.dart
+++
@@ -1,7 +0,0 @@
-part of '../router.dart';
-
-@TypedGoRoute<LicenseRoute>(path: '/licenses')
-class LicenseRoute extends GoRouteData {
-  @override
-  Widget build(context, state) => const LicensePage();
-}
